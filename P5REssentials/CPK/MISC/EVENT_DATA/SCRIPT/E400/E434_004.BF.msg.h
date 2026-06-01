243f1f30c296f90bde726a43f50fa1e1e722531b
theloneraven <ravxnz@proton.me>
2026-06-01 01:40:48 -0400

    initial commit

---
+++ P5REssentials/CPK/MISC/EVENT_DATA/SCRIPT/E400/E434_004.BF.msg.h
@@ -0,0 +1,4 @@
+// Decompiled by Atlus Script Tools
+const int TEST_000                         = 0;
+const int TEST_MINE_00                     = 1;
+const int TEST_MINE2                       = 2;
---
+++ P5REssentials/CPK/MISC/EVENT_DATA/SCRIPT/E400/E485_340.bf.msg.h
@@ -0,0 +1,18 @@
+// Decompiled by Atlus Script Tools
+const int GENERIC_HELP_01                  = 0;
+const int GENERIC_HELP_02                  = 1;
+const int GENERIC_HELP_03                  = 2;
+const int GENERIC_HELP_04                  = 3;
+const int GENERIC_HELP_05                  = 4;
+const int GENERIC_HELP_06                  = 5;
+const int GENERIC_HELP_07                  = 6;
+const int GENERIC_HELP_08                  = 7;
+const int GENERIC_HELP_09                  = 8;
+const int GENERIC_HELP_10                  = 9;
+const int GENERIC_HELP_11                  = 10;
+const int GENERIC_HELP_12                  = 11;
+const int MND_000_0_0                      = 12;
+const int SEL_001_0_0                      = 13;
+const int MND_002_0_0                      = 14;
+const int MND_003_0_0                      = 15;
+const int MND_999_0_0                      = 16;
