daf516d2b8bc919645230be5223aff9cd45a18c9
oceanstuck <msamiclassified@gmail.com>
2026-06-05 02:56:59 -0700

    convert december study event for merging

--- P5REssentials/CPK/MISC/EVENT_DATA/SCRIPT/E400/E434_004.BF.msg.h
+++
@@ -1,4 +0,0 @@
-// Decompiled by Atlus Script Tools
-const int TEST_000                         = 0;
-const int TEST_MINE_00                     = 1;
-const int TEST_MINE2                       = 2;
