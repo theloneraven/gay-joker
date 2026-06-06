a50e9df9f1671b9ea3b9f973de7f8cf307941d1f
oceanstuck <msamiclassified@gmail.com>
2026-06-05 23:45:25 -0700

    fix wrong extension

---
+++ P5REssentials/CPK/MISC/EVENT_DATA/SCRIPT/E700/E702_092.BF.msg.h
@@ -0,0 +1,5 @@
+// Decompiled by Atlus Script Tools
+const int MND_KUDOKITECHNIQUE_00           = 0;
+const int SEL_KUDOKITECHNIQUE_01           = 1;
+const int MND_201_0_0                      = 2;
+const int MND_202_0_0                      = 3;
